5b2a2c1d460ebc89211f1ae8150582563eb62c27
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-11-28 12:11:59 +0530

    Added binarysearch

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class BinarySearch {
+public class BinarySearch {
+
+    public static void binarySearch(int[] array, int top, int bottom, int key) {
+        int mid = (top + bottom) / 2;
+
+        while (top <= bottom) {
+            if (array[mid] > key) {
+                bottom = mid - 1;
+            } else if (array[mid] == key) {
+                System.out.println("Key found at " + mid);
+                break;
+            } else if (array[mid] < key) {
+                top = mid + 1;
+            }
+
+            mid = (top + bottom) / 2;
+        }
+    }
+
 }

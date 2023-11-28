f282fcf3a0ed9818bf495c46d219c0e19e1da57f
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-11-28 12:10:18 +0530

    Update file

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,8 +1,8 @@
 public class LinearSearch {
-    public static int search(int[] myArray, int arrayLength, int key){
+    public static int search(int[] myArray, int arrayLength, int key) {
 
-        for (int i= 0; i < arrayLength; i++){
-            if (myArray[i] == key){
+        for (int i = 0; i < arrayLength; i++) {
+            if (myArray[i] == key) {
                 return i;
             }
         }

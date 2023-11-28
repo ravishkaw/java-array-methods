6f7863e0a197319405f0cce677df92bfbd0fdf09
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-11-28 12:09:43 +0530

    fixed print problem

--- src/PrintArray.java
+++ src/PrintArray.java
@@ -1,7 +1,7 @@
 public class PrintArray {
 
     public static void printArray(int[] myArray, int arrayLength) {
-        for (int i = 0; i < arrayLength - 1; i++) {
+        for (int i = 0; i < arrayLength; i++) {
             System.out.print(myArray[i] + " ");
         }
         System.out.println("\n");
